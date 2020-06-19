739103764e10c4e5a22d300cac5ce5dd024751d8
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-19 04:58:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,25 @@ def batch_badge_creator(attendees)
   end
   badges
 end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  attendees.each.with_index(1) do |name_with_room, index|
+    room_assignments << "Hello, #{name_with_room}! You'll be assigned to room #{index}!"
+  end
+  room_assignments
+end
+
+def printer (attendees)
+
+  badges = batch_badge_creator(attendees)
+  badges.each do |badge|
+    puts badge
+  end
+
+  room_assignments = assign_rooms(attendees)
+  room_assignments.each do |room|
+    puts room
+  end
+
+end
