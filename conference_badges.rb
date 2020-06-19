36252ac9ade39594e13e383c990d66558b7f51e5
Priscilla Leyva <pleyva.co89@gmail.com>
2020-06-19 04:27:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
     "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(attendees)
-  attendees.each do
-    puts "Hello, my name is #{attendees}."
+def batch_badge_creator(attendee_names)
+  attendees_names.each do |name|
+    puts "Hello, my name is #{name}."
   end
 end
